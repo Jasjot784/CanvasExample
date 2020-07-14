422852a22db662c5ad946151434caca48990b54c
Jasjot Singh <jasjot784@gmail.com>
2020-07-14 17:06:09 +0530

    Final Code made

--- app/src/main/java/com/jasjotsingh/canvasexample/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/canvasexample/MainActivity.java
@@ -3,12 +3,17 @@ package com.jasjotsingh.canvasexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+
+        MyCanvasView myCanvasView = new MyCanvasView(this);
+        myCanvasView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(myCanvasView);
     }
 }
